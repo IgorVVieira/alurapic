7e93ea7a9a0d8c9be3224033282c8d603ad2460c
IgorVVieira <igor.gutoo63@gmail.com>
2022-08-22 20:35:48 -0300

    feat: add vee-validate locale

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import VueAxios from 'vue-axios'
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from './routes'
 import { transform } from './directives/Transform'
-import { defineRule } from 'vee-validate';
+import { configure, defineRule } from 'vee-validate';
 import { required, email, min } from '@vee-validate/rules';
+import { localize } from '@vee-validate/i18n';
+import ptBr from '@vee-validate/i18n/dist/locale/pt_PT.json';
 
 const router = createRouter({
   routes,
@@ -18,6 +20,10 @@ axios.defaults.baseURL = 'http://localhost:3000/'
 defineRule('required', required);
 defineRule('email', email);
 defineRule('min', min);
+
+configure({
+  generateMessage: localize('pt_BR', ptBr),
+});
 
 createApp(App)
   .directive('transform', transform)
